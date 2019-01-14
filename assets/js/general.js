87e224b5390d53138bea4f8d894a4a7d2ca1a33d
Marcelo Schneider <schneider.fei@gmail.com>
2019-01-14 16:26:13 -0200

    Primeiro commit

---
+++ assets/js/general.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+        dist: 0,
+        padding: 0,
+        fullWidth:true,
+        indicators: true,
+        duration: 100
+    });
+
+    autoplay();
+});
+
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
